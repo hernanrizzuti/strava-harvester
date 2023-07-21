6904e39aa53d6da5325a45658510b1354d979abb
Hernan Rizzuti <hernanrizzuti@gmail.com>
2023-07-21 11:47:38 +0100

    first commit

---
+++ src/main/java/com/rizzutih/stravaharvester/StravaHarvesterApplication.java
@@ -0,0 +1,13 @@
+package com.rizzutih.stravaharvester;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StravaHarvesterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StravaHarvesterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rizzutih/stravaharvester/StravaHarvesterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rizzutih.stravaharvester;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StravaHarvesterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
